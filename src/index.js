0466ea5629edffce1c758d270a5e85f9b2aa8221
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-18 16:09:34 -0600

    added combineReducers to index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import './index.css';
 import App from './App';
 import {strainsReducer} from './reducers/strainsReducer'
+import { typesReducer } from './reducers/typesReducer';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-const store = createStore(strainsReducer, composeEnhancer(applyMiddleware(thunk)))
+const rootReducer = combineReducers({
+  strains: strainsReducer,
+  types: typesReducer
+})
+
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
 
 root.render(
   <Provider store={store}>
