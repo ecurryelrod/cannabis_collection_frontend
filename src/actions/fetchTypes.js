6bf0e15d0dff91cc55166d8a570a6993ded1d4b0
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-19 14:41:27 -0600

    changed name of fetchTypes action

--- src/actions/fetchTypes.js
+++
@@ -1,8 +0,0 @@
-export const fetchTypes = () => {
-    return dispatch => {
-        dispatch({type: 'ADD_TYPES'})
-        fetch('http://127.0.0.1:3000/api/v1/types')
-        .then(resp => resp.json())
-        .then(types => dispatch({type: 'ADD_TYPES', types}))
-    }
-}
