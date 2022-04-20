5e61ff8573b4f96ef14ee93ca20c40c8074273a5
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-19 18:26:41 -0600

    updated action/reducer and app.js for fetching types

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
-// import StrainForm from './components/strains/StrainForm';
+import StrainForm from './components/strains/StrainForm';
 import { connect } from 'react-redux';
 import { fetchStrains } from './actions/strains';
+import { fetchTypes } from './actions/types';
 import Strains from './components/strains/Strains';
 
 class App extends Component {
@@ -9,11 +10,11 @@ class App extends Component {
     // debugger
     return (
       <div className="App">
-        {/* <StrainForm types={this.props.types} fetchedTypes={this.props.fetchTypes} /> */}
+        <StrainForm getTypes={this.props.fetchTypes()} />
         <Strains getStrains={this.props.fetchStrains()} />
       </div>
     );
   }
 }
 
-export default connect(null, {fetchStrains})(App);
+export default connect(null, {fetchStrains, fetchTypes})(App);
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,8 +1,17 @@
+// synchronous actions
+export const setTypes = types => {
+    return {
+        type: 'SET_TYPES',
+        types
+    }
+}
+
+// asynchronous actions
 export const fetchTypes = () => {
     return dispatch => {
-        dispatch({type: 'ADD_TYPES'})
         fetch('http://127.0.0.1:3000/api/v1/types')
         .then(resp => resp.json())
-        .then(types => dispatch({type: 'ADD_TYPES', types}))
+        .then(types => {debugger})
+        .then(types => dispatch(setTypes(types.data)))
     }
 }
