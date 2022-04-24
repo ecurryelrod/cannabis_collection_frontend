a6a2654be79fe4c7cbb17de13723ed2d715ee87c
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-24 14:51:35 -0600

    created Home.js component

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to Cannabis Collection</h1>
+            <h3>Mission Statement???</h3>
+            <p>Mission statement of app goes here.</p>
+        </div>
+    )
+}
+
+export default Home
