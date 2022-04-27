606c33e5b99edc997eced76ba5a26ec1aca099ca
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-27 16:56:28 -0600

    added form elements to EditStrainForm

--- src/components/strains/EditStrainForm.js
+++ src/components/strains/EditStrainForm.js
@@ -1,13 +1,94 @@
 import React, {Component} from "react";
+import { connect } from "react-redux";
 
 class EditStrainForm extends Component {
+    handleOnChange = e => {
+
+    }
+
+    handleOnSubmit = e => {
+        e.preventDefault()
+    }
+
     render() {
+        const strain = this.props.strains.find(strain => strain.id === this.props.match.params.id)
+        
+        const types = this.props.types.map(type => <option key={type.attributes.id} value={type.attributes.id}>{type.attributes.name}</option>)
+       
+        const effects = this.props.effects.map(effect => (
+            <div key={effect.attributes.id}>
+                <input 
+                    id={effect.attributes.id}
+                    className="checkbox"
+                    type='checkbox' 
+                    value={effect.attributes.id}
+                />
+                <label>{effect.attributes.name}</label>
+            </div>
+        ))
+        
         return (
-            <>
-                Edit Strain Form Comp
-            </>
+            <div>
+                <h2>Edit {strain.attributes.name} Strain</h2>
+                <p>
+                    <strong>Edit Strain Type: </strong>
+                    <select 
+                        value={strain.attributes.type_id} 
+                    >
+                        {types}
+                    </select><br/>
+                </p>
+                <form onSubmit={this.handleOnSubmit}>
+                    <input 
+                        type="text"
+                        name="name"
+                        value={strain.attributes.name}
+                        onChange={this.handleOnChange}
+                    /><br/>
+                    <input 
+                        type="text"
+                        name="description"
+                        value={strain.attributes.description}
+                        onChange={this.handleOnChange}
+                    /><br/>
+                    <input 
+                        type="text"
+                        name="terpene"
+                        value={strain.attributes.terpene}
+                        onChange={this.handleOnChange}
+                    /><br/>
+                    <input 
+                        type="text"
+                        name="thc_amount"
+                        value={strain.attributes.thc_amount}
+                        onChange={this.handleOnChange}
+                    /><br/>
+                    <input 
+                        type="text"
+                        name="cbd_amount"
+                        value={strain.attributes.cbd_amount}
+                        onChange={this.handleOnChange}
+                    /><br/>
+                    <input 
+                        type="text"
+                        name="cbg_amount"
+                        value={strain.attributes.cbg_amount}
+                        onChange={this.handleOnChange}
+                    /><br/>
+                    <p><strong>Edit Effects Experienced:</strong></p>
+                    <div className="formEffects">{effects}</div>
+                    <input type="submit" />
+                </form>
+                {/* {this.state.name} */}
+            </div>
         )
     }
 }
 
-export default EditStrainForm
+const mapStateToProps = state => ({
+    strains: state.strains,
+    types: state.types,
+    effects: state.effects
+})
+
+export default connect(mapStateToProps)(EditStrainForm)
