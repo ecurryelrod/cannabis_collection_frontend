a303137b2e28fcda5868778ab5d4514825a62157
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-27 11:51:02 -0600

    updated routes and connect()

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, {Component} from 'react';
-import StrainForm from './components/strains/StrainForm';
+import NewStrainForm from './components/strains/NewStrainForm';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { fetchStrains } from './actions/strains';
-import { fetchTypes } from './actions/types';
-import { fetchEffects } from './actions/effects';
+// import { fetchStrains } from './actions/strains';
+// import { fetchTypes } from './actions/types';
+// import { fetchEffects } from './actions/effects';
 import Strains from './components/strains/Strains';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
@@ -24,24 +24,13 @@ class App extends Component {
           <Navbar />
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/strains" render={() => <Strains getStrains={this.props.fetchStrains()} />} />
-            <Route exact path="/strains/new" render={({history}) => <StrainForm history={history} getTypes={this.props.fetchTypes()} getEffects={this.props.fetchEffects()} />} />
-            {/* <Route exact path="/">
-              <Home />
-            </Route>
-            <Route exact path="/strains">
-              <Strains getStrains={this.props.fetchStrains()} />
-            </Route>
-            <Route exact path="/strains/new">
-              <StrainForm getTypes={this.props.fetchTypes()} getEffects={this.props.fetchEffects()} />
-            </Route> */}
+            <Route exact path="/strains" component={Strains} />
+            <Route exact path="/strains/new" component={NewStrainForm} />
           </Switch>
         </Router>
-        {/* <StrainForm getTypes={this.props.fetchTypes()} getEffects={this.props.fetchEffects()} /> */}
-        {/* <Strains getStrains={this.props.fetchStrains()} /> */}
       </div>
     );
   }
 }
 
-export default connect(null, {fetchStrains, fetchTypes, fetchEffects})(App);
+export default connect()(App);
