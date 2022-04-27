0c9bcff460644cc57e131cdfaf118732711128cc
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-27 15:27:00 -0600

    bare bones EditStrainForm component

---
+++ src/components/strains/EditStrainForm.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class EditStrainForm extends Component {
+    render() {
+        return (
+            <>
+                Edit Strain Form Comp
+            </>
+        )
+    }
+}
+
+export default EditStrainForm
