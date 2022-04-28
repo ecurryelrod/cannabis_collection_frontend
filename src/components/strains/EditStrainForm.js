eafa7daca714e8b04cb8837f2e753fe28a09ee77
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-28 11:20:34 -0600

    added state to EditStrainForm

--- src/components/strains/EditStrainForm.js
+++ src/components/strains/EditStrainForm.js
@@ -2,50 +2,67 @@ import React, {Component} from "react";
 import { connect } from "react-redux";
 
 class EditStrainForm extends Component {
-    handleOnChange = e => {}
+    state = {
+        name: this.props.strain.attributes.name,
+        description: this.props.strain.attributes.description,
+        terpene: this.props.strain.attributes.terpene,
+        thc_amount: this.props.strain.attributes.thc_amount,
+        cbd_amount: this.props.strain.attributes.cbd_amount,
+        cbg_amount: this.props.strain.attributes.cbg_amount,
+        type_id: this.props.strain.attributes.type.id,
+        effect_ids: this.props.strain.attributes.effects
+    }
+
+    handleOnChange = e => this.setState({[e.target.name]: e.target.value})
+
+    handleCheckedEffects = e => {
+        debugger
+    }
 
     handleOnSubmit = e => {
         e.preventDefault()
     }
 
     render() {
-        const { strain, strainType, strainEffects } = this.props
+        // debugger
 
         const types = this.props.types.map(type => {
-            // debugger
-            // if (type.id === strainType) {
             return <option 
                     key={type.attributes.id} 
                     value={type.attributes.id}
                 >
                     {type.attributes.name}
                 </option>
-            // }
-            // return <option key={type.attributes.id} value={type.attributes.id}>{type.attributes.name}</option>
         })
        
-        const effects = this.props.effects.map(effect => (
+        const effects = this.props.effects.map(effect => {
+            return (
             <div key={effect.attributes.id}>
                 <input 
                     id={effect.attributes.id}
                     className="checkbox"
                     type='checkbox' 
                     value={effect.attributes.id}
+                    // onChange={this.handleCheckedEffects}
+                    // defaultChecked={this.handleCheckedEffects(strainEffects)}
                 />
                 <label>{effect.attributes.name}</label>
             </div>
-        ))
+
+            )
+        })
         
         return (
             <div>
-                <h2>Edit {strain.attributes.name} Strain</h2>
+                <h2>Edit {this.state.name} Strain</h2>
 
                 <form onSubmit={this.handleOnSubmit}>
                     <p>
                         <strong>Edit Strain Type: </strong>
                         <select 
-                            defaultValue={strainType}
-                            value={strain.attributes.type_id} 
+                            name="type_id"
+                            defaultValue={this.state.type_id}
+                            onChange={this.handleOnChange}
                         >
                             {types}
                         </select><br/>
@@ -53,44 +70,45 @@ class EditStrainForm extends Component {
                     <input 
                         type="text"
                         name="name"
-                        value={strain.attributes.name}
+                        value={this.state.name}
                         onChange={this.handleOnChange}
                     /><br/>
                     <input 
                         type="text"
                         name="description"
-                        value={strain.attributes.description}
+                        value={this.state.description}
                         onChange={this.handleOnChange}
                     /><br/>
                     <input 
                         type="text"
                         name="terpene"
-                        value={strain.attributes.terpene}
+                        value={this.state.terpene}
                         onChange={this.handleOnChange}
                     /><br/>
                     <input 
                         type="text"
                         name="thc_amount"
-                        value={strain.attributes.thc_amount}
+                        value={this.state.thc_amount}
                         onChange={this.handleOnChange}
                     /><br/>
                     <input 
                         type="text"
                         name="cbd_amount"
-                        value={strain.attributes.cbd_amount}
+                        value={this.state.cbd_amount}
                         onChange={this.handleOnChange}
                     /><br/>
                     <input 
                         type="text"
                         name="cbg_amount"
-                        value={strain.attributes.cbg_amount}
+                        value={this.state.cbg_amount}
                         onChange={this.handleOnChange}
                     /><br/>
                     <p><strong>Edit Effects Experienced:</strong></p>
                     <div className="formEffects">{effects}</div>
                     <input type="submit" />
                 </form>
-                {/* {this.state.name} */}
+                {this.state.name}
+                {console.log(this.state)}
             </div>
         )
     }
