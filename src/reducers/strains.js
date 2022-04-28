443ece94347f3259cfed07e0fa01f5fbdc08c9ab
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-04-28 16:01:29 -0600

    added UPDATE_STRAIN action to reducer

--- src/reducers/strains.js
+++ src/reducers/strains.js
@@ -1,14 +1,15 @@
 export const strains = (state = [], action) => {
-    // debugger
     switch (action.type) {
-        case 'SET_STRAINS': return action.strains
+        case 'SET_STRAINS': 
+            return action.strains
 
-        case 'ADD_STRAIN': return [...state, action.strain]
+        case 'ADD_STRAIN': 
+            return [...state, action.strain]
         
-        // delete does not re-render page without deleted strain
-        // must refresh page to see update
+        case 'UPDATE_STRAIN': 
+            return state.map(strain => strain.attributes.id === action.strain.attributes.id ? action.strain : strain)
+         
         case 'DELETE_STRAIN': 
-            // debugger
             return state.filter(strain => strain.attributes.id !== action.strainId)
 
         default: return state
