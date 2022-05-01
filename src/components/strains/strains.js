7eb52f9dd3d171d125bda1524086b14a47323fb8
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-05-01 15:46:09 -0600

    added div for styling and removed comments

--- src/components/strains/strains.js
+++ src/components/strains/strains.js
@@ -1,21 +1,24 @@
 import React, {Component} from "react";
 import { connect } from "react-redux";
 import Strain from "./Strain";
-// import { fetchStrains } from "../../actions/strains";
 import { editStrain } from "../../actions/strains";
 import { deleteStrain } from "../../actions/strains";
 
 class Strains extends Component {
-    // componentDidMount() {
-    //     this.props.fetchStrains()
-    // }
     render() {
         return (
             <div>
                 <h2>Strains</h2>
-                {this.props.strains.map(strain => 
-                    <Strain key={strain.id} strain={strain.attributes} edit={this.props.editStrain} delete={this.props.deleteStrain} />
-                )}
+                <div className="strainContainer">
+                    {this.props.strains.map(strain => 
+                        <Strain 
+                            key={strain.id} 
+                            strain={strain.attributes} 
+                            edit={this.props.editStrain} 
+                            delete={this.props.deleteStrain} 
+                        />
+                    )}
+                </div>
             </div>
         )
     }
