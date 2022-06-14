53d65e87148ced6937b8d75dbeb1fd218deb7be3
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-06-13 19:04:55 -0600

    added loggedIn conditional for NavBar/Routers in App.js

--- src/App.js
+++ src/App.js
@@ -16,34 +16,43 @@ import SignUpForm from './components/SignUpForm';
 class App extends Component {
   componentDidMount() {
     this.props.fetchEffects()
-    // this.props.fetchStrains()
     this.props.fetchTypes()
     this.props.getCurrentUser()
   }
 
   render() {
+    if (this.props.loggedIn) {
+      return (
+        <div className="App">
+          <Router>
+            <Navbar />
+            {/* Switch comp makes sure only one route matches at the same time */}
+            <Switch>
+              <Route exact path="/strains" component={Strains} />
+              <Route exact path="/strains/new" component={NewStrainForm} />
+              <Route exact path="/strains/:id/edit" render={renderProps => {
+                const strain = this.props.strains.addedStrains.find(strain => strain.id.toString() === renderProps.match.params.id)
+                
+                return <EditStrainForm 
+                  strain={strain} 
+                  strainEffects={strain.effects.map(effect => {
+                    return effect.id.toString()})} 
+                  strainType={strain.type.id}
+                  {...renderProps} 
+                />
+              }} />
+            </Switch>
+          </Router>
+        </div>
+      )
+    }
     return (
       <div className="App">
         <Router>
-          <Navbar />
           {/* Switch comp makes sure only one route matches at the same time */}
           <Switch>
             <Route exact path="/" render={routeProps => {
               return <Home {...routeProps} />
-            }} />
-            <Route exact path="/strains" component={Strains} />
-            <Route exact path="/strains/new" component={NewStrainForm} />
-            <Route exact path="/strains/:id/edit" render={renderProps => {
-              const strain = this.props.strains.addedStrains.find(strain => strain.id.toString() === renderProps.match.params.id)
-              // debugger
-              return <EditStrainForm 
-                strain={strain} 
-                strainEffects={strain.effects.map(effect => {
-                  // debugger
-                  return effect.id.toString()})} 
-                strainType={strain.type.id}
-                {...renderProps} 
-              />
             }} />
             <Route exact path="/login" render={renderProps => {
               return <LoginForm {...renderProps} />
@@ -60,7 +69,7 @@ class App extends Component {
 
 const mapStateToProps = state => ({
   strains: state.strains,
-  currentUser: state.currentUser
+  loggedIn: !!state.currentUser
 })
 
 export default connect(mapStateToProps, {fetchEffects, fetchStrains, fetchTypes, getCurrentUser})(App);
